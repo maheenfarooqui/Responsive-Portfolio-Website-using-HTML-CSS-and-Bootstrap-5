cf80a93025f67afced06e35a99a03b46b366f988
maheen <maheenzuhra@gmail.com>
2026-04-28 13:21:14 +0500

    add changes

---
+++ app.js
@@ -0,0 +1,14 @@
+var typed = new Typed('#typed', {
+  strings: [
+    'Frontend React Developer', 
+    'eCommerce UI',
+    'Landing Pages',
+    'Firebase',
+    'WordPress Specialist'
+  ],
+  typeSpeed: 50,      // Likhte waqt ki speed
+  backSpeed: 30,      // Mitaate waqt ki speed
+  backDelay: 2000,    // Ek sentence khatam hone ke baad kitni dair ruke (2 seconds)
+  loop: true,         // Dobara start ho jaye
+  cursorChar: '|',    // Cursor ka design
+});
